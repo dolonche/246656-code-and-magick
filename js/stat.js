eee69285b723370120f4317f64be0244936d2eb5
dolonche <ivan.pitenin@gmail.com>
2017-08-16 17:49:50 +0300

    Всё исправил

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,23 @@
+var positionX;
+var positionY;
+var widthBlock;
+var heightblock;
+var colorBlock;
+function paintBlock(ctx, positionX, positionY, widthBlock, heightblock, colorBlock) {
+	ctx.fillStyle = colorBlock;
+	ctx.fillRect(positionX, positionY, widthBlock, heightblock);
+}
 window.renderStatistics = function (ctx, names, times) {
-	ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-	ctx.fillRect(110, 20, 420, 270);
-	ctx.fillStyle = 'rgba(256, 256, 256, 1)';
-	ctx.strokeRect(100, 10, 420, 270);
-	ctx.fillRect(100, 10, 420, 270);
+	paintBlock(ctx, 110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
+	paintBlock(ctx, 100, 10, 420, 270, 'rgba(256, 256, 256, 1)')
+//	paintBlock('(110, 20, 420, 270)', 'rgba(0, 0, 0, 0.7)');
+//	paintBlock('(100, 10, 420, 270)', 'rgba(256, 256, 256, 1)');
+	//	ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+	//	ctx.fillRect(110, 20, 420, 270);
+	//	ctx.fillStyle = 'rgba(256, 256, 256, 1)';
+	//	ctx.strokeRect(100, 10, 420, 270);
+	//	ctx.fillRect(100, 10, 420, 270);
+//	paintBlock(ctx, 110, 20, 420, 270, black);
 	ctx.fillStyle = '#000';
 	ctx.font = '16px PT Mono';
 	ctx.fillText('Ура вы победили!', 120, 40);
@@ -20,24 +34,17 @@ window.renderStatistics = function (ctx, names, times) {
 	var histogramHeight = 150;
 	var step = histogramHeight / (max - 0);
 	var maxOpacity = 1;
-	var minOpacity = 0;
-	var randomOpacity = Math.random() * (maxOpacity - minOpacity) + minOpacity;
-	//	ctx.fillRect(120, 80, 40, times[0] * step);
-	//	ctx.fillRect(210, 80, 40, times[1] * step);
-	//	ctx.fillRect(300, 80, 40, times[2] * step);
-	//	for (var i = 0; i < times.length; i++) {
-	//		var timeLevel = Math.round(times[i]);
-	//		ctx.fillText(timeLevel, 120 + 80 * i, 0);
-	//	}
+	var minOpacity = 0.2;
 	ctx.textBaseline = 'top'; // Рисуем надпись от левого верхнего угла
 	for (var i = 0; i < names.length; i++) {
 		if (names[i] === 'Вы') {
 			ctx.fillStyle = '#ff0000';
-			console.log(names[i]);
 		} else {
+			var randomOpacity = Math.random() * (maxOpacity - minOpacity) + minOpacity;
 			ctx.fillStyle = 'rgba(0, 0, 255,' + randomOpacity + ')';
 		}
 		ctx.fillRect(120 + 80 * i, 2 * histogramHeight - times[i] * step - 60, 40, times[i] * step);
+		ctx.fillStyle = '#000';
 		ctx.fillText(names[i], 120 + 80 * i, 60 + histogramHeight + 40);
 		var timeLevel = Math.round(times[i]);
 		ctx.fillText(timeLevel, 120 + 80 * i, 2 * histogramHeight - times[i] * step - 80);
